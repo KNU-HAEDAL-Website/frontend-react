5800a863330fe150d3d2b315306cc75fc070221a
ajin <aj5210@naver.com>
2025-01-12 23:21:25 +0900

    Chore: tanstack query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { ReactQueryClientProvider } from './lib/query-client'
 import { Router } from './routes'
 
 function App() {
   return (
-    <>
+    <ReactQueryClientProvider>
       <Router />
-    </>
+    </ReactQueryClientProvider>
   )
 }
 
